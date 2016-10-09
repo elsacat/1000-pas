f1444b24dc0299a58956baf851750223a4d56ad0
elsaclschneider@gmail.com <elsaclschneider@gmail.com>
2016-10-09 19:51:08 +0200

    first commit
    first commit

---
+++ app/src/main/java/ch/elsachneider/a10000pas/marcher.java
@@ -0,0 +1,13 @@
+package ch.elsachneider.a10000pas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class marcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marcher);
+    }
+}
